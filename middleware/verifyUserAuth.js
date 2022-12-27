84e7ff70e6a2e82af21d852626b2d1551ffa5a99
Sidben99 <sidben9991@gmail.com>
2022-12-27 11:00:22 +0100

    seperate the auth handlerand use one on get and one on post requests

--- middleware/verifyUserAuth.js
+++ middleware/verifyUserAuth.js
@@ -1,9 +1,15 @@
-const verifyAuthHandler = (req , res , next) => {
-    let {username , role} = req.cookies;
-    if(!username) username = null;
-    res.locals.username = username;
+const verifyAuthOnGet = (req , res , next) => {
+    const {username , role , userId} = req.cookies;
+    res.locals.username = username ?? null;
     res.locals.role = role;
+    res.locals.userId = userId;
     next();
 }
 
-module.exports = verifyAuthHandler;
+const verifyAuthOnPost = (req , res , next) => {
+    const {username , role , userId} = req.cookies;
+    if(username && role && userId) next()
+    else return res.json({"message":"err"});
+}
+
+module.exports = {verifyAuthOnGet , verifyAuthOnPost};
